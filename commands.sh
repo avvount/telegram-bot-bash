92e166fec29ab56602222ab7bb4183423ed43316
avvount <1447696903@qq.com>
2018-02-15 14:22:59 +0800

    修改：     commands.sh

--- commands.sh
+++ commands.sh
@@ -52,7 +52,7 @@ else
 			if [[ "${USER[ID]}" == "290582222" && "${CHAT[ID]}" == "290582222" ]]; then
 				for i in $HOME/file/*; do
 					if [[ ${i##*.} = "png" ]]; then
-						send_file "${CHAT[ID]}" "$HOME/file/$i" "端口 ： $i"
+						send_file "${CHAT[ID]}" "$i" "端口 ： $i"
 					fi
 				done
 			fi
